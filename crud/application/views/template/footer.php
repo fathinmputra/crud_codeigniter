a14e9976a210b8ef385a4955aed084adf9532f27
Fathin Muhashibi Putra <fathinmp11@gmail.com>
2023-11-20 19:09:46 +0700

    feat add: CRUD Code Igniter Data Karyawan

---
+++ crud/application/views/template/footer.php
@@ -0,0 +1,9 @@
+	<footer class="footer">
+	  <div class="container" style="text-align:center;">
+	    <hr />
+	    <span>Fathin Muhashibi Putra - 5025211229 - PBKK A <?php echo date('Y'); ?></span>
+	  </div>
+	</footer>
+	</body>
+
+	</html>
